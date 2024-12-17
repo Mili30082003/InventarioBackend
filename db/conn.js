a762f1c154b871f11421999e4a17b9b4189bfea9
Mili Antoni <148805721+Mili30082003@users.noreply.github.com>
2024-12-16 21:45:34 -0300

    vercel

--- db/conn.js
+++
@@ -1,9 +0,0 @@
-require('dotenv').config();
-const mongoose = require('mongoose');
-
-const DB = process.env.MONGO_URI;
-
-mongoose
-  .connect(DB)
-  .then(() => console.log("Conectado a Mongo"))
-  .catch((error) => console.log(error.message));
